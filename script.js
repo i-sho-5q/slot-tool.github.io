fda31584f4965da89c808287b9738230d7eb244e
i-sho-5q <162408514+i-sho-5q@users.noreply.github.com>
2026-03-10 08:19:24 +0900

    for_pwa

--- script.js
+++ script.js
@@ -45,3 +45,13 @@ function calculateExchange() {
 }
 
 calculateExchange();
+
+// PWA化
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js")
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
